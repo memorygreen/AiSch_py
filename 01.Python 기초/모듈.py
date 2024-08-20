3a53e1418cfa28222c9470e7e94375318864fbbc
김자영 <108516942+memorygreen@users.noreply.github.com>
2024-08-20 22:59:09 +0900

    인공지능사관학교 파이썬 관련 수업 내용 커밋 (라이브러리부터 합류)

---
+++ 01.Python 기초/모듈.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+A = 1 
+B = 2
+
+def add(*args):
+    return sum(args)
+
+
+# In[ ]:
+
+
+get_ipython().system('jupyter nbconvert --to script 04.모듈.ipynb')
+
+# ! -> python 언어가 아닌 프롬프트 명령어(스크립트문)
+# jupyter nbconvert : 주피터 파일(ipynb)을 다른 형식으로 변활 할 수 있는 명령어(기본으로 .py로 변환)
+# -- to : 타겟을 지정
+# script 파일명 : 타겟의 형식
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
